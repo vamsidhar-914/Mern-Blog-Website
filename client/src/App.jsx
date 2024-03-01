9db801f3d685c6ee7d3cfff98f578b52b8a6ebb7
vamsi <vamsimay12345@gmail.com>
2024-03-01 19:43:55 +0530

    added files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter , Routes ,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import Singup from "./pages/Singup"
+import Dashboard from "./pages/Dashboard"
+import Header from "./components/Header"
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>app</h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/login" element={<Signin />}/>
+        <Route path="/register" element={<Singup />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
