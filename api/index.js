a3ff2b84d3ef2e29481d00bd69a555e44dfeaef4
vamsi <vamsimay12345@gmail.com>
2024-03-09 02:44:03 +0530

    added files

--- api/index.js
+++ api/index.js
@@ -35,10 +35,10 @@ app.use("/api/users" , userRoutes)
 app.use("/api/posts" , postRoutes)
 app.use("/api/comments" , commentRoutes)
 
-app.use(express.static(path.join(__dirname , '/client/dist')))
-app.get("*" , (req,res) => {
-    res.sendFile(path.join(__dirname ,'client' , 'dist' , 'index.html'))
-})
+// app.use(express.static(path.join(__dirname , '/client/dist')))
+// app.get("*" , (req,res) => {
+//     res.sendFile(path.join(__dirname ,'client' , 'dist' , 'index.html'))
+// })
 
 // middleware for error handler
 app.use((err,req,res,next) => {
