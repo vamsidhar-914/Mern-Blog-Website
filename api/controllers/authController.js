17e00c1878a6349d0f126a92d5805ba14f012619
vamsidhar-914 <vamsimay12345@gmail.com>
2024-04-02 09:29:06 +0530

    added files

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,100 +1,120 @@
-const { createError } = require("../middleware/error")
-const bcrypt = require('bcrypt')
-const User = require("../models/User")
-const jwt = require('jsonwebtoken')
+const { createError } = require("../middleware/error");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 // register api
-const register = async(req, res,next) => {
-    const { username , email ,password } = req.body
-    if(!username || !email || !password || username === "" || email === '' || password === ''){
-        return next(createError(400 , "All fields are required"))
-    }
+const register = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return next(createError(400, "All fields are required"));
+  }
 
-    const foundUser = await User.findOne({ username })
-    const foundEmail = await User.findOne({ email })
-    
-    if(!foundUser || !foundEmail){
-        return next(createError(400 , 'user already exists'))
-    }
+  const foundUser = await User.findOne({ username });
+  const foundEmail = await User.findOne({ email });
 
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(req.body.password , salt)
+  if (foundUser || foundEmail) {
+    return next(createError(400, "user already exists"));
+  }
 
-    const newUser = new User({
-        username,
-        email,
-        password : hash
-    })
-    try{
-        await newUser.save()
-        res.status(200).json("reigstered succesfully")
-    }catch(error){
-        next(error)
-    }
-}
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hash,
+  });
+  try {
+    await newUser.save();
+    res.status(200).json("reigstered succesfully");
+  } catch (error) {
+    next(error);
+  }
+};
 
 //loginapi
-const login = async(req,res,next) => {
-    const { email , password } = req.body
-    if(!email || !password || email ==='' || password === ""){
-        // res.status(400).json({ message : "All fields are required"})
-        return next(createError(400 , "All fields are required"))
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    // res.status(400).json({ message : "All fields are required"})
+    return next(createError(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(createError(404, "User not found"));
     }
-    try{
-        const validUser = await User.findOne({ email })
-        if(!validUser){
-            return next(createError(404,"User not found"))
-        }
-        const match = await  bcrypt.compare(password , validUser.password)
-        if(!match){
-            return next(createError(400 ,'invalid password'))
-        }
-        const token = jwt.sign(
-            {
-                id : validUser._id,
-                isAdmin : validUser.isAdmin
-            }, process.env.SECRET
-        )
-        const { password : pass , ...others } = validUser._doc
-        res.status(200).cookie('access_token',token,{
-            httpOnly : true
-        }).json(others)
-    }catch(err){
-        next(err)
+    const match = await bcrypt.compare(password, validUser.password);
+    if (!match) {
+      return next(createError(400, "invalid password"));
     }
-} 
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+        isAdmin: validUser.isAdmin,
+      },
+      process.env.SECRET
+    );
+    const { password: pass, ...others } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(others);
+  } catch (err) {
+    next(err);
+  }
+};
 
-const google = async(req,res,next) => {
-    const { name  , email , googlePhotoUrl} = req.body
-    try{
-        const user = await User.findOne({ email })
-        if(user){
-            const token = jwt.sign({ id : user._id } , process.env.SECRET)
-            const {password , ...rest } = user._doc
-            res.status(200).cookie('access_token' , token , {
-                httpOnly : true
-            }).json(rest)
-        } else{
-            const generatePass = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8)
-            const salt = bcrypt.genSaltSync(10)
-            const hash = bcrypt.hashSync(generatePass , salt)
-            const newUser = new User({
-                username : name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
-                email,
-                password : hash,
-                profilePicture : googlePhotoUrl
-            })
-            await newUser.save()
-            const token = jwt.sign({ id : newUser._id} , process.env.SECRET)
-            const { password , ...rest } = newUser._doc
-            res.status(200).cookie("access_token" , token , {
-                httpOnly : true,
-            }).json(rest)
-        }
-    }catch(err){
-        next(err)
+const google = async (req, res, next) => {
+  const { name, email, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.SECRET);
+      const { password, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    } else {
+      const generatePass =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(generatePass, salt);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hash,
+        profilePicture: googlePhotoUrl,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.SECRET);
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
     }
-}
+  } catch (err) {
+    next(err);
+  }
+};
 
-
-module.exports = { register,login,google} 
+module.exports = { register, login, google };
--- api/index.js
+++ api/index.js
@@ -1,56 +1,59 @@
-require('dotenv').config()
-const express = require("express")
-const mongoose = require('mongoose')
-const app = express()
-const AuthRoutes = require('./routes/auth')
-const cookieParser = require('cookie-parser')
-const userRoutes = require("./routes/users")
-const postRoutes = require("./routes/posts")
-const commentRoutes = require("./routes/comments")
-const path = require("path")
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const AuthRoutes = require("./routes/auth");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/users");
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
+const path = require("path");
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log('mongoDB is connected')
-})
-.catch((err) => {
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("mongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // middleware
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
 
-app.listen(3000,() => {
-    console.log("server is running on port 3000");
-})
-
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
 
 // app.use("/api/user" , userRoutes)
-app.use("/api/auth" , AuthRoutes)
-app.use("/api/users" , userRoutes)
-app.use("/api/posts" , postRoutes)
-app.use("/api/comments" , commentRoutes)
+app.use("/api/auth", AuthRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
+// last
 // app.use(express.static(path.join(__dirname , '/client/dist')))
 // app.get("*" , (req,res) => {
 //     res.sendFile(path.join(__dirname ,'client' , 'dist' , 'index.html'))
 // })
-app.use(express.static(path.join(__dirname ,'../client/dist')))
-app.get("/*" , (req,res,) => {
-    res.sendFile(path.join(__dirname , "../client/dist"))
-})
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist"));
+});
 
 // middleware for error handler
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error"
-    res.status(statusCode).json({
-        success : false,
-        statusCode,
-        message
-    })
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
